05a5f006f22fce2866a05b9892f21a2e440f9c80
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2021-12-02 10:08:33 +0700

    update home(belum selesai)

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Index extends CI_Controller
+class Home extends CI_Controller
 {
 
     function __construct()
--- application/views/v_home.php
+++ application/views/v_home.php
@@ -16,11 +16,37 @@
 
     <?php
     $url = 'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=4a3d81b73e3e4cfbaac57c3e390a1c5d';
+    $response = file_get_contents($url);
+    $newsData = json_decode($response);
     ?>
 
     <div class="jumbotron">
         <h1> DJY News</h1>
     </div>
+
+    <div class="container-fluid" <?php
+                                    foreach ($newsData->articles as $news) {
+                                    ?> <div class="row NewsGrid">
+        <div class="col-md-3">
+            <img src="<?php $news->urlToImage ?>" alt="News thumbnail">
+        </div>
+        <div class="col-md-9">
+            <h2>Title: <?php echo $news->title ?></h2>
+            <h5>Description: <?php echo $news->description ?></h5>
+            <p>Content: <?php echo $news->content ?> </p>
+            <h6>Author: <?php echo $news->author ?> </h6>
+            <h6>Published: <?php echo $news->publishedAt ?></h6>
+        </div>
+    </div>
+<?php
+                                    }
+?>
+
+
+</div>
+
+
+
 </body>
 
 </html>
