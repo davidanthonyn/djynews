9a67a849e578d245bb78a25ff9cdc80d79b9068d
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2021-12-14 18:35:51 +0700

    newsportal to djynews

--- application/views/includes/config.php
+++ application/views/includes/config.php
@@ -2,11 +2,10 @@
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
+define('DB_NAME','djynews');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>
